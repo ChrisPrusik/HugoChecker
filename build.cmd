ba3ba802b4580218fe1071c583a36219aa288004
chrisprusik <chris@g1ant.com>
2023-08-07 13:41:35 +0200

    build.cmd to execute on mac

--- build.cmd
+++ build.cmd
@@ -1,17 +1,5 @@
 dotnet publish src/arghul-hugo-checker.sln -c Release -o dist/win-x64/ -r win-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true
 
-if ($LASTEXITCODE -ne 0) {
-    exit $LASTEXITCODE
-}
-
 dotnet publish src/arghul-hugo-checker.sln -c Release -o dist/linux-x64/ -r linux-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true
 
-if ($LASTEXITCODE -ne 0) {
-    exit $LASTEXITCODE
-}
-
 dotnet publish src/arghul-hugo-checker.sln -c Release -o dist/osx-x64/ -r osx-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true
-
-if ($LASTEXITCODE -ne 0) {
-    exit $LASTEXITCODE
-}
