063d1604f4afda7e320cc50438877d4ca2fbca20
chrisprusik <chris@g1ant.com>
2023-08-09 13:12:19 +0200

    Update CheckerService.cs

--- src/CheckerService.cs
+++ src/CheckerService.cs
@@ -11,6 +11,8 @@ public class CheckerService
     private readonly Core core;
 
     public string HugoFolder => core.GetInput("hugo-folder");
+    public string HugoConfigFile => Path.Combine(HugoFolder, "config.yaml");
+    public string CheckerConfigFile => Path.Combine(HugoFolder, "arghul-hugo-checker.yaml");
     
     public CheckerService(Core core)
     {
@@ -28,9 +30,6 @@ public class CheckerService
     
     private async Task ReadConfig()
     {
-        var fileName = Path.Combine(HugoFolder, "arghul-hugo-checker.yaml");
-        if (!File.Exists(fileName))
-            throw new Exception($"File '{fileName}' does not exist");
     }
     
     private void CheckInputs()
@@ -40,6 +39,12 @@ public class CheckerService
         
         if (!Directory.Exists(HugoFolder))
             throw new Exception($"Directory input:hugo-folder: '{HugoFolder}' does not exist");
+        
+        if (!File.Exists(HugoConfigFile))
+            throw new Exception($"Hugo config file: '{HugoConfigFile}' does not exist");
+
+        if (!File.Exists(CheckerConfigFile))
+            throw new Exception($"Hugo checker config file: '{CheckerConfigFile}' does not exist");
     }
 
     private void StartInformation()
