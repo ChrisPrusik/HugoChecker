1a6d53f6c63356013afa97d466d015b564c014d0
chrisprusik <chris@g1ant.com>
2023-08-24 02:10:16 +0200

    new dist packages

--- build.cmd
+++ build.cmd
@@ -1,3 +1,5 @@
-dotnet publish src/HugoChecker.sln -c Release -o dist/win-x64/ -r win-x64
-dotnet publish src/HugoChecker.sln -c Release -o dist/linux-x64/ -r linux-x64
-dotnet publish src/HugoChecker.sln -c Release -o dist/osx-x64/ -r osx-x64
+dotnet publish src/HugoChecker.sln -c Release -o dist/win-x64/ -r win-x64 -p:PublishSingleFile=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true
+
+dotnet publish src/HugoChecker.sln -c Release -o dist/linux-x64/ -r linux-x64 -p:PublishSingleFile=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true
+
+dotnet publish src/HugoChecker.sln -c Release -o dist/osx-x64/ -r osx-x64 -p:PublishSingleFile=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true
