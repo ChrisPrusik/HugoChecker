920be90ba98f5641f750c953b5c7bf042dfadec1
chrisprusik <chris@prusik.info>
2026-02-08 12:04:04 +0100

    Migrate `build.cmd` to `build.sh` and add bash shebang for script execution.

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 dotnet publish src/HugoChecker.sln -c Release -o dist/win-x64/ -r win-x64 -p:PublishSingleFile=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true
 
 dotnet publish src/HugoChecker.sln -c Release -o dist/linux-x64/ -r linux-x64 -p:PublishSingleFile=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true
